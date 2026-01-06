c27988adedc2429a94d6c6d8e0839e53eccaf37f
Syedar <1270261172@qq.com>
2026-01-06 22:26:15 +0800

    feat: 完善dataset实体定义
    - 添加id、name和description字段
    - 引入Datasource实体并建立一对一关系

--- apps/server/src/module/dataset/entities/dataset.entity.ts
+++ apps/server/src/module/dataset/entities/dataset.entity.ts
@@ -1,1 +1,27 @@
-export class Dataset {}
+import { Datasource } from 'src/module/datasource/entities/datasource.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('dataset')
+export class Dataset {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  name: string;
+
+  @Column({
+    type: 'varchar',
+    nullable: true,
+  })
+  description?: string;
+
+  @OneToOne(() => Datasource)
+  @JoinColumn()
+  datasource: Datasource;
+}
